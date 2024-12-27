c1fed75250e87ff43c18ea81eb036f3af28dc02e
shanubabu <70108773+shanubabu@users.noreply.github.com>
2024-12-27 17:06:33 +0530

    Update module-info.java

--- demo1/src/main/java/module-info.java
+++ demo1/src/main/java/module-info.java
@@ -1,13 +1,1 @@
-module com.example.demo1 {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires google.cloud.translate;
-    requires org.apache.httpcomponents.core5.httpcore5;
-    requires org.seleniumhq.selenium.api;
-    requires org.seleniumhq.selenium.chrome_driver;
-    requires org.seleniumhq.selenium.support;
 
-
-    opens com.example.demo1 to javafx.fxml;
-    exports com.example.demo1;
-}
